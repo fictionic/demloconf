b8137edcc76dbfe580e77c374d95e940e604a05c
dylandforbes <dylandforbes@gmail.com>
2017-08-18 17:39:02 -0700

    toggle for using various artists naming scheme

---
+++ .config/demlo/scripts/02-varartist.lua
@@ -0,0 +1,9 @@
+---------------
+-- 02: TOOLS --
+---------------
+
+-- allows filename to indicate that the track
+-- has an artist different from the album artist
+-- even when o.album_artist ~= "Various Artists"
+
+various_artists_format_always_allowed = true
